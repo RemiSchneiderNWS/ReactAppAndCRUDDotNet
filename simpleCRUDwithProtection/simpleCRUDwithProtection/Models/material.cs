634ef82232dedcd386693d399a90149e205b58b0
RemiSchneider76 <remischneider76@gmail.com>
2021-05-12 16:31:47 +0200

    add back&front

---
+++ simpleCRUDwithProtection/simpleCRUDwithProtection/Models/material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpleCRUDwithProtection.Models
+{   
+    [Table("material")]
+    public class material
+    {
+        [Key]
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
